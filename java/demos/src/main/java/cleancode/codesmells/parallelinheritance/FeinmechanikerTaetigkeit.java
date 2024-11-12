ea4266af53c7873c91c87c388a9734496d37371a
Tom <tom@ohms.pro>
2024-11-12 09:11:05 +0100

    feat(demo+uebung): Parallel Inheritance

---
+++ java/demos/src/main/java/cleancode/codesmells/parallelinheritance/FeinmechanikerTaetigkeit.java
@@ -0,0 +1,7 @@
+package cleancode.codesmells.parallelinheritance;
+
+public class FeinmechanikerTaetigkeit extends Taetigkeit {
+    String get() {
+        return "Kümmert sich um die feinen Sachen";
+    }
+}
---
+++ java/demos/src/main/java/cleancode/codesmells/parallelinheritance/Taetigkeit.java
@@ -0,0 +1,5 @@
+package cleancode.codesmells.parallelinheritance;
+
+public abstract class Taetigkeit {
+    abstract String get();
+}
